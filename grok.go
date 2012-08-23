1e8047989deed870e0d382ea0aa9a1a872def7d1
Blake Smith <blakesmith0@gmail.com>
2012-08-22 22:50:02 -0500

    Add compilation

--- grok.go
+++ grok.go
@@ -6,8 +6,21 @@ package grok
 */
 import "C"
 
+import (
+	"errors"
+	"fmt"
+)
+
+const (
+	GROK_OK = iota
+)
+
 type Grok struct {
 	g *C.grok_t
+}
+
+type Match struct {
+	gm *C.grok_match_t
 }
 
 func New() *Grok {
@@ -18,4 +31,14 @@ func New() *Grok {
 	}
 
 	return grok
+}
+
+func (grok *Grok) Compile(pattern string) error {
+	p := C.CString(pattern)
+	ret := C.grok_compilen(grok.g, p, C.int(len(pattern)))
+	if ret != GROK_OK {
+		return errors.New(fmt.Sprintf("Failed to compile: %s", C.GoString(grok.g.errstr)))
+	}
+
+	return nil
 }
--- grok_test.go
+++ grok_test.go
@@ -10,3 +10,12 @@ func TestNew(t *testing.T) {
 		t.Fatal("Failed to initialize grok")
 	}
 }
+
+func TestDayCompile(t *testing.T) {
+	g := New()
+	pattern := "%{DAY}"
+	err := g.Compile(pattern)
+	if err != nil {
+		t.Fatal("Error:", err)
+	}
+}
